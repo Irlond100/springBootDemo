e83c64635bb18fbb3a0ee91acca8a82ce7cb6db8
Irlond100 <irlondito100@gmail.com>
2023-08-05 22:44:58 +0300

    Error Correction

--- src/main/java/ru/netology/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/ru/netology/springbootdemo/SpringBootDemoApplication.java
@@ -4,21 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
+import ru.netology.springbootdemo.model.DevProfile;
+import ru.netology.springbootdemo.model.ProductionProfile;
+import ru.netology.springbootdemo.model.SystemProfile;
 
-@ConditionalOnProperty("netology.profile.dev")
+
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
-    }
-    @Bean
-    public SystemProfile devProfile() {
-        return new DevProfile();
-    }
-
-    @Bean
-    public SystemProfile prodProfile() {
-        return new ProductionProfile();
     }
 }
